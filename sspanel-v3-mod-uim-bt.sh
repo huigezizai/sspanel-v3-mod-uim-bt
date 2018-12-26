99ebffdfd74588dde8c615613154656437cfef4d
七彩blog <35214016+lizhongnian@users.noreply.github.com>
2018-12-27 00:25:28 +0800

    Update sspanel-v3-mod-uim-bt.sh

--- sspanel-v3-mod-uim-bt.sh
+++ sspanel-v3-mod-uim-bt.sh
@@ -1,6 +1,32 @@
 #!/bin/bash
-#Check Root
+##检查操作系统
+check_sys(){
+	if [[ -f /etc/redhat-release ]]; then
+		release="centos"
+	elif cat /etc/issue | grep -q -E -i "debian"; then
+		release="debian"
+	elif cat /etc/issue | grep -q -E -i "ubuntu"; then
+		release="ubuntu"
+	elif cat /etc/issue | grep -q -E -i "centos|red hat|redhat"; then
+		release="centos"
+	elif cat /proc/version | grep -q -E -i "debian"; then
+		release="debian"
+	elif cat /proc/version | grep -q -E -i "ubuntu"; then
+		release="ubuntu"
+	elif cat /proc/version | grep -q -E -i "centos|red hat|redhat"; then
+		release="centos"
+    fi
+	bit=`uname -m`
+}
+
+##检查是否root用户
 [ $(id -u) != "0" ] && { echo -e " “\033[31m Error: 必须使用root用户执行此脚本！\033[0m”"; exit 1; }
+##定义常用属性
+Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
+Info="${Green_font_prefix}[信息]${Font_color_suffix}"
+Error="${Red_font_prefix}[错误]${Font_color_suffix}"
+Tip="${Green_font_prefix}[注意]${Font_color_suffix}"
+
 #宝塔sspanel-v3-mod-uim快速部署工具
 echo -e "感谢使用 “\033[32m 宝塔sspanel-v3-mod-uim快速部署工具 \033[0m”"
 echo "----------------------------------------------------------------------------"
@@ -12,9 +38,11 @@ read -p "请输入宝塔面板添加的MySQL用户名(数据库名)：" mysqluse
 echo $mysqlusername
 echo "请输入宝塔面板添加的MySQL密码：" mysqlpassword
 echo $mysqlpassword
+echo "请输入网站的mukey(用于webapi方式对接后端，可以自定义)：" sspanelmukey
+echo $sspanelmukey
 sleep 1
 echo "请等待系统自动操作......"
-cd /www/wwwroot/$web
+cd /www/wwwroot/$website
 rm -rf index.html 404.html
 ##安装git unzip
 yum install git unzip -y
@@ -37,4 +65,14 @@ chmod -R 755 *
 chown -R www:www storage
 php composer.phar install
 echo "location / {try_files \$uri \$uri/ /index.php\$is_args\$args;}"> /www/server/panel/vhost/rewrite/$website.conf
-sed -i 's/root /www/wwwroot/www.baidu.com;/root /www/wwwroot/www.baidu.com/public;/g' /www/server/panel/vhost/nginx/$website.conf
+sed -i 's/root /www/wwwroot/$website;/root /www/wwwroot/$website/public;/g' /www/server/panel/vhost/nginx/$website.conf
+cd /www/wwwroot/$website
+cp config/.config.php.for7color config/.config.php
+sed -i 's/websiteurl/$website/g' /www/wwwroot/$website/config/.config.php
+sed -i 's/sspanel-mukey/$sspanelmukey/g' /www/wwwroot/$website/config/.config.php
+sed -i 's/sspanel-db-databasename/$mysqlusername/g' /www/wwwroot/$website/config/.config.php
+sed -i 's/sspanel-db-username/$mysqlusername/g' /www/wwwroot/$website/config/.config.php
+sed -i 's/sspanel-db-password/$mysqlpassword/g' /www/wwwroot/$website/config/.config.php
+
+
+
