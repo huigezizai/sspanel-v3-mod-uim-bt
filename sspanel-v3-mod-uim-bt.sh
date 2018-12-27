c7045110f21edc93de1310dd6e2b5611b987305f
七彩blog <35214016+lizhongnian@users.noreply.github.com>
2018-12-27 11:25:58 +0800

    Update sspanel-v3-mod-uim-bt.sh

--- sspanel-v3-mod-uim-bt.sh
+++ sspanel-v3-mod-uim-bt.sh
@@ -32,15 +32,27 @@ echo -e "感谢使用 “\033[32m 宝塔sspanel-v3-mod-uim快速部署工具 \03
 echo "----------------------------------------------------------------------------"
 echo -e "请注意这些要求:“\033[31m 宝塔版本=5.9 \033[0m”，添加网址PHP版本必须选择为“\033[31m PHP7.1 \033[0m”,添加完成后地址不要改动！"
 echo "----------------------------------------------------------------------------"
-read -p "请输入宝塔面板添加的网站域名,请不要修改添加之后的默认地址（例如:www.baidu.com，不带http/https）：" website
-echo $website
-read -p "请输入宝塔面板添加的MySQL用户名(数据库名)：" mysqlusername
-echo $mysqlusername
-echo "请输入宝塔面板添加的MySQL密码：" mysqlpassword
-echo $mysqlpassword
-echo "请输入网站的mukey(用于webapi方式对接后端，可以自定义)：" sspanelmukey
-echo $sspanelmukey
+stty erase '^H' && read -p "请输入宝塔面板添加的网站域名,请不要修改添加之后的默认地址（例如:www.baidu.com，不带http/https）：" website
+stty erase '^H' && read -p "请输入宝塔面板添加的MySQL用户名(数据库名)：" mysqlusername
+stty erase '^H' && echo -p "请输入宝塔面板添加的MySQL密码：" mysqlpassword
+stty erase '^H' && echo -p "请输入网站的mukey(用于webapi方式对接后端，可以自定义)：" sspanelmukey
 sleep 1
+echo -e "${Info} 请确认您输入的网站域名：$website"
+echo -e "${Info} 请确认您输入的MySQL用户名：$mysqlusername"
+echo -e "${Info} 请确认您输入的MySQL密码：$mysqlpassword"
+echo -e "${Info} 请确认您输入的mukey：$sspanelmukey"
+stty erase '^H' && read -p " 请输入数字(1：继续；2：退出) [1/2]:" status
+case "$status" in
+	1)
+	echo -e "${Info} 您选择了继续，开始安装程序！"
+	;;
+	2)
+	echo -e "${Tip} 您选择了退出，请重新执行安装程序！" && exit 1
+	;;
+	*)
+	echo -e  "${Error} 请输入正确值 [1/2]，请重新执行安装程序" && exit 1
+	;;
+esac
 echo -e "${Info} 请等待系统自动操作......"
 cd /www/wwwroot/$website
 rm -rf index.html 404.html
